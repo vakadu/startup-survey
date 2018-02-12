95585d82125a16ab4a181e38c66c5be7db40cb93
vakadu <vakadu10@gmail.com>
2018-02-12 22:52:15 +0530

    finished oauth flow

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: '745078855911-1s1ukamos0psm9huvq7obken8js3h4g8.apps.googleusercontent.com',
+    googleClientSecret: 'KmzpjTZ2tmcgjldk_gEz0r5R',
+    mongoURI: 'mongodb://vakadu10:sachin10@ds035290.mlab.com:35290/emaily-dev',
+    cookieKey: 'asdfghjklqwertyuiopzxcvbnm'
+}
+
+//don't commit this
---
+++ services/passport.js
@@ -0,0 +1,40 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const keys = require('../config/keys');
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+})
+
+passport.use(new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if (existingUser){
+                    //we already have a record with id
+                    done(null, existingUser );
+                }
+                else {
+                    //we don't have a record with given id
+                    new User({ googleId: profile.id }).save()
+                        .then(user => done(null, user))
+                }
+            })
+        // console.log("Access Token", accessToken);
+        // console.log("Refresh Token", refreshToken);
+        // console.log("Profile", profile);
+    })
+);
