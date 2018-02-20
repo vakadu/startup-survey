c6e3559d24f2dd587e9960ce6a765e0ccadf804b
vakadu <vakadu10@gmail.com>
2018-02-20 22:44:52 +0530

    added materialize css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './header';
+
+// const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>Survey New</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
     return (
-        <div>
-            Hello There!!!!
+        <div className="container">
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route exact path="/" component={Landing}/>
+                    <Route exact path="/surveys" component={Dashboard}/>
+                    <Route path="/surveys/new" component={SurveyNew}/>
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
