3fd6d44ebecc14ad6ab951f74b2d85eecb29b3ab
vakadu <vakadu10@gmail.com>
2018-02-27 21:16:39 +0530

    added header navigation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Header from './header';
+import Landing from './landing';
+import * as actions from '../actions';
 
 // const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>Survey New</h2>
-const Landing = () => <h2>Landing</h2>
+// const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-    return (
-        <div className="container">
-            <BrowserRouter>
-                <div>
-                    <Header/>
-                    <Route exact path="/" component={Landing}/>
-                    <Route exact path="/surveys" component={Dashboard}/>
-                    <Route path="/surveys/new" component={SurveyNew}/>
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+class App extends Component{
+    componentDidMount(){
+        this.props.fetchUser();
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header/>
+                        <Route exact path="/" component={Landing}/>
+                        <Route exact path="/surveys" component={Dashboard}/>
+                        <Route path="/surveys/new" component={SurveyNew}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default connect(null, actions) (App);
